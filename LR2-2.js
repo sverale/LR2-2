8a7423f19e34b5a49daa2795c2559f99557af9ce
sverale <112919812+sverale@users.noreply.github.com>
2023-01-29 22:07:46 +0700

    Add files via upload

--- LR2-2.js
+++ LR2-2.js
@@ -1,22 +1,47 @@
 let arr = [p1, p2, p3, p4, p5, p6]
+    var k=1;//шаг
+    for(i=0;i<k;i++) arr.unshift(arr.pop());
+    console.log(arr);
 
 // alert(arr.length)
-for (let i = 0; i < arr.length; i++) {
+// for (let i = 0; i < arr.length; i++) {
  
-    arr[i] = arr[i+1]
-    console.log( arr[i] );
+    // arr[i] = arr[i+1]
+    // console.log( arr[i] );
+/////////////////////////////
+
+// var a=[1,2,3,45,8,9,7,10]
 
 
-//     var a=[1,2,3,45,8,9,7,10]
-// var k=3;//шаг
-// for(i=0;i<k;i++) a.unshift(a.pop());
-// console.log(a);
+// ////////////////////////////
+
+// var array = []
+var array = document.getElementsByTagName('[arr]');
+for (var i = 0; i < [arr].length; i++) {
+
+  array = arr[i].innerHTML;
+  console.log(arr)
 }
+
+
+
+////////////////////////////////////
+
+// let td = document.querySelectorAll('#dayWrap td');
+
+// var result = Array.from(td).map((e) => {
+//   return e.innerHTML;
+// });
+
+// console.log(result);
+
+////////////////////////////////
+// }
 
 // btn1.onclick = function() {
 //     attack()  
 // }
 
 // function attack() {
-
+    
 // }
