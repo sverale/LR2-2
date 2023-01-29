fe0114548c4c36c69ca6da2753ca0dbb919831cb
sverale <112919812+sverale@users.noreply.github.com>
2023-01-29 21:50:06 +0700

    Add files via upload

--- LR2-2.js
+++ LR2-2.js
@@ -1,12 +1,22 @@
 let arr = [p1, p2, p3, p4, p5, p6]
 
-alert(arr.length)
+// alert(arr.length)
+for (let i = 0; i < arr.length; i++) {
+ 
+    arr[i] = arr[i+1]
+    console.log( arr[i] );
 
+
+//     var a=[1,2,3,45,8,9,7,10]
+// var k=3;//шаг
+// for(i=0;i<k;i++) a.unshift(a.pop());
+// console.log(a);
+}
 
 // btn1.onclick = function() {
 //     attack()  
 // }
 
-function attack() {
+// function attack() {
 
-}
+// }
