a951b0025234e19ab33518899d2b4d5c77494f33
sverale <112919812+sverale@users.noreply.github.com>
2023-02-01 20:47:51 +0700

    Add files via upload

--- LR2-2.js
+++ LR2-2.js
@@ -1,47 +1,130 @@
-let arr = [p1, p2, p3, p4, p5, p6]
-    var k=1;//шаг
-    for(i=0;i<k;i++) arr.unshift(arr.pop());
-    console.log(arr);
 
-// alert(arr.length)
-// for (let i = 0; i < arr.length; i++) {
  
-    // arr[i] = arr[i+1]
-    // console.log( arr[i] );
-/////////////////////////////
+function jumpTeam1() {
+  let arr1 = [p1_1, p2_1, p3_1, p4_1, p5_1, p6_1]
+  let array1 = []
+  console.log(arr1);
+  for(let i = 0; i < 6; i++) {
+    array1.push (arr1[i].innerText) 
+    console.log(array1); 
+  }
 
-// var a=[1,2,3,45,8,9,7,10]
+  var k=1;//шаг
+  for(i=0;i<k;i++) array1.unshift(array1.pop());
+    console.log(array1);
+  for (let i = 0; i < 6; i++){
+    arr1[i].innerHTML = array1[i];
+  }
+}
 
-
-// ////////////////////////////
-
-// var array = []
-var array = document.getElementsByTagName('[arr]');
-for (var i = 0; i < [arr].length; i++) {
-
-  array = arr[i].innerHTML;
-  console.log(arr)
+function jumpTeam2() { 
+  arr2 = [p1_2, p2_2, p3_2, p4_2, p5_2, p6_2]
+  let array2 = []
+  console.log(arr2);
+  for(let i = 0; i < 6; i++) {
+    array2.push ( arr2[i].innerText)
+    console.log(array2);
+  }
+  
+  var k=1;//шаг
+  for(i=0;i<k;i++) array2.unshift(array2.pop());
+    console.log(array2);
+  for (let i = 0; i < 6; i++){
+    arr2[i].innerHTML = array2[i];
+  }
 }
 
 
 
-////////////////////////////////////
 
-// let td = document.querySelectorAll('#dayWrap td');
+// alert(`первый розыгрыш подает игрок ${pitcher.innerHTML}`)
+attack_1.onclick = function () {
+  jumpTeam1()
+}
+attack_2.onclick = function () {
+  jumpTeam2()
+}
 
-// var result = Array.from(td).map((e) => {
-//   return e.innerHTML;
-// });
+// let player = prompt('кто начал?')
+// function attack () {
+// } 
 
-// console.log(result);
+// function block () {}
+block_1.onclick = function() {
+  alert(`Команда "1" отбила. Мяч идет дальше`)
+  // block()
+}
+block_2.onclick = function() {
+  alert(`Команда "2" отбила. Мяч идет дальше`)
+  // block()
+}
 
-////////////////////////////////
+
+
+
+// function pitch() {}
+pitch_1.onclick = function () {
+  let arr1 = [p1_1, p2_1, p3_1, p4_1, p5_1, p6_1]
+  let array1 = []
+  for(let i = 0; i < 6; i++) {
+    array1.push (arr1[i].innerText) 
+  }
+  alert(`Игрок ${array1[4]} подал мяч`)
+  
+}
+pitch_2.onclick = function () {
+  let arr2 = [p1_2, p2_2, p3_2, p4_2, p5_2, p6_2]
+  let array2 = []
+  for(let i = 0; i < 6; i++) {
+    array2.push (arr2[i].innerText) 
+  }
+  alert(`Игрок ${array2[1]} подал мяч`)
+}
+
+
+
+// function error () {
+//   //
+// }
+//error
+error_1.onclick = function() {
+  alert(`Команда "1" допустила ошибку. Мяч переходит противнику. Команда "2"  получает 1 балл`)
+  counter2()
+}
+
+error_2.onclick = function() {
+  alert(`Команда "2" допустила ошибку. Мяч переходит противнику. Команда "1"  получает 1 балл`)
+  
+  counter1()
+}
+
+
+
+
+// function change () {
+  
 // }
 
-// btn1.onclick = function() {
-//     attack()  
-// }
 
-// function attack() {
-    
-// }
+
+let count1 = 0 
+//////////   count   /////////////////////
+
+function counter1 () {
+  
+  if (count1 < 25) {
+  let count1 = ct1.innerHTML
+  // ct1.innerHTML = count1;
+  count1 ++
+  console.log(count1)
+  }
+  
+}
+
+function counter2 () {
+  for (let i = 0; i < 25; i++) {
+  let count2 = ct2.innerHTML
+  // ct2.innerHTML = count2;
+  }
+  // count2 ++
+}
